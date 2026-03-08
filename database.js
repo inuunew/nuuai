6dcd73a72ddbdc6ce6e0b2c4a40d374754b40fb8
inuunew <choirulazzamibnu@gmail.com>
2026-03-09 00:05:22 +0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,44 @@
+const database = {
+
+ff:{
+name:"Free Fire",
+type:"id",
+image:"assets/ff.png",
+items:[
+{name:"5 Diamond Free Fire",price:"Rp1.130"},
+{name:"12 Diamond Free Fire",price:"Rp2.070"},
+{name:"50 Diamond Free Fire",price:"Rp6.638"}
+]
+},
+
+ml:{
+name:"Mobile Legends",
+type:"id",
+image:"assets/ml.png",
+items:[
+{name:"86 Diamond ML",price:"Rp20.000"},
+{name:"172 Diamond ML",price:"Rp40.000"}
+]
+},
+
+pubg:{
+name:"PUBG Mobile",
+type:"voucher",
+image:"assets/pubg.png",
+items:[
+{name:"60 UC Voucher",price:"Rp15.000"},
+{name:"325 UC Voucher",price:"Rp75.000"}
+]
+},
+
+genshin:{
+name:"Genshin Impact",
+type:"uid",
+image:"assets/genshin.png",
+items:[
+{name:"60 Genesis Crystal",price:"Rp16.000"},
+{name:"300 Genesis Crystal",price:"Rp79.000"}
+]
+}
+
+}
