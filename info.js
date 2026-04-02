5d5026b7024e2567ed70bd54fae5d8f494f0f2ec
inuunew <choirulazzamibnu@gmail.com>
2026-04-02 17:01:10 +0700

    Create info.js

---
+++ info.js
@@ -0,0 +1,33 @@
+const infoData = [
+
+  {
+    title: "Inuu Market Admin",
+    date: "31 maret 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "https://files.catbox.moe/b629ct.png",
+    desc: "Liburan selesai nih...moga aja masih ada niat buat lanjutin website nya yaa dikit dikit soalnya belajar juga buat bentar lagi ujian. :/",
+    tags: "#InuuTyzDev"
+  },
+
+{
+    title: "Inuu Market Admin",
+    date: "21 maret 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Selamat lebaran ya! mohon maaf lahir batin.",
+    tags: "#InuuTyzDev"
+  }, 
+
+  {
+    title: "Inuu Market Admin",
+    date: "18 maret 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Yoo minna liburan sekolah niatnya sih gw mau bikin Website store gitu moga aja jadi ya...cuma gabut aja sih buatnya juga:)",
+    tags: "#InuuTyzDev"
+  }
+
+];
