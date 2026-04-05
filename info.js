b7bc0d579cf53c6a128a69c371320a1ba4d22fb3
inuunew <choirulazzamibnu@gmail.com>
2026-04-05 19:14:56 +0700

    Update info.js

--- info.js
+++ info.js
@@ -12,12 +12,51 @@ const infoData = [
   
   {
     title: "InuuMarket Admin",
+    date: "21 MARET 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Selamat hari raya idul Fitri mohon maaf lahir batin",
+    tags: "#InuuTyzDev"
+  },
+  
+  {
+    title: "InuuMarket Admin",
     date: "31 MARET 2026",
     type: "INFO",
     image: "https://files.catbox.moe/tv1rv6.jpeg",
     banner: "https://files.catbox.moe/b629ct.png",
     desc: "Liburan selesai...Moga aja masih ada niat lanjutin soalnya banyak kegiatan juga apalagi mau ujian akhir,yaa.... upgrade dikit-dikit la.",
     tags: "#InuuTyzDev"
-  }
+  },
+  
+  {
+    title: "InuuMarket Admin",
+    date: "2 April 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Music page telah ditambahkan, udah bisa dengerin music nih...boleh req nambah lagu loh ke admin...",
+    tags: "#InuuTyzDev"
+  },
+  
+  {
+    title: "InuuMarket Admin",
+    date: "4 April 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Update Sistem SPA(Single Page Aplication)! website lebih lancar saat pindah halaman dibanding sebelumnya,music tidak mati walau pindah halaman(kecuali ke bagian checkout)/hp mati/keluar chrome",
+    tags: "#InuuTyzDev"
+  },
+  {
+    title: "InuuMarket Admin",
+    date: "5 April 2026",
+    type: "INFO",
+    image: "https://files.catbox.moe/tv1rv6.jpeg",
+    banner: "",
+    desc: "Update halaman Kalkulator MLBB buat yang bingung kalo ngedraw bilang "aduh diamond segini cukup ga ya" ngecek cukup enggak nya bisa di halaman Kalkulator MLBB ya biar tau!!",
+    tags: "#InuuTyzDev"
+  },
 
 ];
